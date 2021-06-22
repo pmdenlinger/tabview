555df654d9674fe7e31f15c202f6311113aee186
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-21 19:20:25 -0700

    Initial Commit

---
+++ tabview/tabviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  tabviewApp.swift
+//  tabview
+//
+//  Created by user on 6/21/21.
+//
+
+import SwiftUI
+
+@main
+struct tabviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
