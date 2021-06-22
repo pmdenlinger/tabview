31beca97cd04c46cb0b2b7cb538dc20d6e36ff1b
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-21 19:29:14 -0700

    Tav view code added.

--- tabview/ContentView.swift
+++ tabview/ContentView.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var selectedTab = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+//        Selection will store and update current tab value
+        TabView(selection: $selectedTab) {
+            
+            Color.red
+//            Tab item view
+                .tabItem {
+                    Image(systemName: "house.fill")
+                }
+                .tag(0)
+            
+            Color.blue
+//            Tab item view
+                .tabItem {
+                    Image(systemName: "suit.heart.fill")
+                }
+                .tag(1)
+            
+            Color.yellow
+//            Tab item view
+                .tabItem {
+                    Image(systemName: "gear")
+                }
+                .tag(2)
+                }
+        }
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
